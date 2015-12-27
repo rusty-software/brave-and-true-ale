6275dab190951dd28a6c63cf452c462696576187
Rusty Bentley <mycoding.alert@gmail.com>
2015-12-27 10:44:58 -0600

    add initial validations and error message fetcher

--- src/brave_and_true_ale/core.clj
+++ src/brave_and_true_ale/core.clj
@@ -1,6 +1,17 @@
 (ns brave-and-true-ale.core
   (:gen-class))
 
+(def order-details-validations
+  {:name ["Please enter a name" not-empty]
+   :email ["Please enter an email address" not-empty
+           "Your email address doesn't look like an email address" #(or (empty? %) (re-seq #"@" %))]})
+
+(defn error-messages-for
+  "Returns a seq of error messages"
+  [to-validate message-validator-pairs]
+  (map first (filter #(not ((second %) to-validate))
+                     (partition 2 message-validator-pairs))))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
--- test/brave_and_true_ale/core_test.clj
+++ test/brave_and_true_ale/core_test.clj
@@ -2,6 +2,11 @@
   (:require [clojure.test :refer :all]
             [brave-and-true-ale.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest error-messages-for-test
+  (testing "Returns errors for missing or malformed fields"
+    (is (= ["Please enter a name"] (error-messages-for "" (:name order-details-validations))))
+    (is (= ["Please enter an email address"] (error-messages-for "" (:email order-details-validations))))
+    (is (= ["Your email address doesn't look like an email address"] (error-messages-for "foobar.baz" (:email order-details-validations)))))
+  (testing "Returns empty collection for fields that pass validation"
+    (is (empty? (error-messages-for "Test Name" (:name order-details-validations))))
+    (is (empty? (error-messages-for "test@name.com" (:email order-details-validations))))))
